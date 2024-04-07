8eddff4b65f8b18c17d4543e88353015fe6ca482
Gokul9114 <dhanushselvaraj1523@gmail.com>
2024-04-07 10:52:17 +0530

    Spotify Dapp Created

---
+++ data/songs.js
@@ -0,0 +1,26 @@
+export const songs = [
+  {
+    index: 0,
+    title: "Ambient Gold",
+    artiste: "Lo-Fi",
+    plays: "20,382",
+    songLength: "2:43",
+    album: "Songs & Beats",
+    cover:
+      "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
+    arweaveLink:
+      "https://tnrapvewequ7ofz7idqmz6xlqmfkplivoyfosxagcpg23rxe.arweave.net/m2IH1JYkKfcXP0DgzPrrgwqnrRV2CulcBhPN_rc_bkY",
+  },
+  {
+    index: 1,
+    title: "Kill it anyway",
+    artiste: "SteamBeats originals",
+    plays: "1,324",
+    songLength: "4:50",
+    album: "The Random House",
+    cover:
+      "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    arweaveLink:
+      "https://rnvytwtlgebo5e57kjl5ax3omeow423vmeul25dwmgueyhst.arweave.net/i2uJ2msxAu6Tv1JX0F9uYR1ua_-3VhKL10dmGoTB5TA",
+  },
+];
